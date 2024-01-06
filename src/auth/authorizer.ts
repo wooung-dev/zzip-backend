04b6f3b4ca5b1a9933a18c3e341bb2a02e3353ed
grr1203 <gyrms9412@naver.com>
2024-01-06 19:39:53 +0900

    Authorizer 정의 추가

--- src/auth/authorizer.ts
+++ src/auth/authorizer.ts
@@ -1,12 +1,6 @@
 import { APIGatewayProxyEventV2 } from 'aws-lambda';
 import { verifyJwtKey } from '../lib/jwt';
 
-export const apiSpec = {
-  category: 'auth',
-  method: 'Authorizer',
-  desc: 'lambda에 붙여서 서버에서 발급한 jwt를 검증한다.',
-};
-
 export const handler = async (event: APIGatewayProxyEventV2): Promise<{ isAuthorized: boolean; context: any }> => {
   let response = { isAuthorized: false, context: {} };
   const tokenFromRequest = event.headers.authorization?.split(' ')[1];
