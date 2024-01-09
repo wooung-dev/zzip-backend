6e7104fb8cab8d3f5b166d12aa1d314d5f28017a
keeut <keeut@naver.com>
2024-01-09 19:16:57 +0900

    follow api added

--- __tests__/task.test.ts
+++ __tests__/task.test.ts
@@ -1,12 +1,15 @@
-import { generateTokens, USER_JWT_CONTENTS } from '../src/lib/jwt';
-import mysqlUtil from '../src/lib/mysqlUtil';
-import * as authorizer from '../src/auth/authorizer';
-import { APIGatewayProxyEventV2, APIGatewayProxyEventV2WithLambdaAuthorizer } from 'aws-lambda';
-import { handler as getUser } from '../src/user/get';
-import { handler as putUser } from '../src/user/put';
-import util from 'util';
+import { generateTokens, USER_JWT_CONTENTS } from "../src/lib/jwt";
+import mysqlUtil from "../src/lib/mysqlUtil";
+import * as authorizer from "../src/auth/authorizer";
+import { APIGatewayProxyEventV2, APIGatewayProxyEventV2WithLambdaAuthorizer } from "aws-lambda";
+import { handler as getUser } from "../src/user/get";
+import { handler as putUser } from "../src/user/put";
+import { handler as postFollow } from "../src/follow/post";
+import { handler as getFollow } from "../src/follow/get";
+import { handler as deleteFollow } from "../src/follow/delete";
+import util from "util";
 
-describe('ZZip test', () => {
+describe("ZZip test", () => {
   // test('GET test', async () => {
   //   const parameters = { name: 's', age: 12 };
   //   const res = await getTest(parameters, createPublicLambdaEvent(parameters));
@@ -14,19 +17,32 @@ describe('ZZip test', () => {
   //   expect(res).toHaveProperty('statusCode', 200);
   // });
 
-  test.only('GET user', async () => {
+  test.only("GET user", async () => {
     const response = await privateFunctionTest(getUser, {});
-    expect(response).toHaveProperty('statusCode', 200);
+    expect(response).toHaveProperty("statusCode", 200);
   });
 
-  test('PUT user', async () => {
+  test("PUT user", async () => {
     const response = await privateFunctionTest(putUser, { profileImage: true });
-    expect(response).toHaveProperty('statusCode', 200);
+    expect(response).toHaveProperty("statusCode", 200);
+  });
+
+  test("POST follow", async () => {
+    const response = await privateFunctionTest(postFollow, { followedUserIdx: 2 });
+    expect(response).toHaveProperty("statusCode", 200);
+  });
+  test("GET follow", async () => {
+    const response = await privateFunctionTest(getFollow, {});
+    expect(response).toHaveProperty("statusCode", 200);
+  });
+  test("DELETE follow", async () => {
+    const response = await privateFunctionTest(deleteFollow, { followedUserIdx: 2 });
+    expect(response).toHaveProperty("statusCode", 200);
   });
 });
 
-async function localLogin(userEmail: string = 'gyrms9412@test.com'): Promise<string> {
-  const user = await mysqlUtil.getOne('tb_user', USER_JWT_CONTENTS, { user_email: userEmail });
+async function localLogin(userEmail: string = "gyrms9412@test.com"): Promise<string> {
+  const user = await mysqlUtil.getOne("tb_user", USER_JWT_CONTENTS, { user_email: userEmail });
   return (await generateTokens(createPublicLambdaEvent({}), user!.idx)).accessToken;
 }
 
@@ -40,29 +56,29 @@ async function privateFunctionTest(testFunction, parameters: { [key: string]: an
 
 const createPublicLambdaEvent = (parameters: { [key: string]: any }): APIGatewayProxyEventV2 => {
   return {
-    version: '2.0',
-    routeKey: '',
-    rawPath: '',
-    rawQueryString: '',
+    version: "2.0",
+    routeKey: "",
+    rawPath: "",
+    rawQueryString: "",
     headers: {},
     queryStringParameters: parameters,
     body: JSON.stringify(parameters),
     requestContext: {
-      accountId: 'testAccountId',
-      apiId: 'testApiId',
-      domainName: 'test.execute-api.ap-northeast-2.amazonaws.com',
-      domainPrefix: 'test',
+      accountId: "testAccountId",
+      apiId: "testApiId",
+      domainName: "test.execute-api.ap-northeast-2.amazonaws.com",
+      domainPrefix: "test",
       http: {
-        method: '',
-        path: '/',
-        protocol: 'HTTP/1.1',
-        sourceIp: '127.0.0.1',
-        userAgent: 'local',
+        method: "",
+        path: "/",
+        protocol: "HTTP/1.1",
+        sourceIp: "127.0.0.1",
+        userAgent: "local",
       },
-      requestId: '',
-      routeKey: '',
-      stage: 'dev',
-      time: '1/Jan/2024:00:00:00 +0000',
+      requestId: "",
+      routeKey: "",
+      stage: "dev",
+      time: "1/Jan/2024:00:00:00 +0000",
       timeEpoch: 0,
     },
     pathParameters: {},
@@ -79,29 +95,29 @@ const createPrivateLambdaEvent = async (
     headers: { authorization: `Bearer ${userAccessToken}` },
   });
   return {
-    version: '2.0',
-    routeKey: '',
-    rawPath: '',
-    rawQueryString: '',
+    version: "2.0",
+    routeKey: "",
+    rawPath: "",
+    rawQueryString: "",
     headers: { authorization: `Bearer ${userAccessToken}` },
     queryStringParameters: parameters,
     body: JSON.stringify(parameters),
     requestContext: {
-      accountId: 'testAccountId',
-      apiId: 'testApiId',
-      domainName: 'test.execute-api.ap-northeast-2.amazonaws.com',
-      domainPrefix: 'test',
+      accountId: "testAccountId",
+      apiId: "testApiId",
+      domainName: "test.execute-api.ap-northeast-2.amazonaws.com",
+      domainPrefix: "test",
       http: {
-        method: '',
-        path: '/',
-        protocol: 'HTTP/1.1',
-        sourceIp: '127.0.0.1',
-        userAgent: 'local',
+        method: "",
+        path: "/",
+        protocol: "HTTP/1.1",
+        sourceIp: "127.0.0.1",
+        userAgent: "local",
       },
-      requestId: '',
-      routeKey: '',
-      stage: 'dev',
-      time: '1/Jan/2023:00:00:00 +0000',
+      requestId: "",
+      routeKey: "",
+      stage: "dev",
+      time: "1/Jan/2023:00:00:00 +0000",
       timeEpoch: 0,
       authorizer: { lambda: { ...jwtClaims } },
     },
---
+++ src/follow/post.ts
@@ -0,0 +1,21 @@
+import { APIGatewayProxyEventV2WithLambdaAuthorizer } from "aws-lambda";
+import mysqlUtil from "@lib/mysqlUtil";
+import { FromSchema } from "json-schema-to-ts";
+
+const parameter = {
+  type: "object",
+  properties: {
+    followedUserIdx: { type: "string" },
+  },
+  required: ["user"],
+} as const;
+
+export const handler = async (event: APIGatewayProxyEventV2WithLambdaAuthorizer<{ [key: string]: any }>) => {
+  console.log("[event]", event);
+  const { followedUserIdx } = JSON.parse(event.body) as FromSchema<typeof parameter>;
+  const userIdx = event.requestContext.authorizer.lambda.idx;
+
+  await mysqlUtil.create("tb_follow", { follower_idx: userIdx, followed_idx: followedUserIdx });
+
+  return { statusCode: 200, body: { followedUser: followedUserIdx } };
+};
